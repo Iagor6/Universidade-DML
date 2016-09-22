00a549016be385acb7873c94c2c2189b6fa1a81f
Iago Almeida <iagocb300r@gmail.com>
2016-09-21 22:48:22 -0300

    Inserindo Valores as tabelas

---
+++ Universidade-DML.sql
@@ -0,0 +1,14 @@
+insert into pessoas values ('612.698.713-66', 'Tasara', 'M');
+insert into pessoas values ('551.078.620-55', 'Menra', 'F');
+insert into pessoas values ('186.091.095-50', 'Ealjack', 'M');
+insert into pessoas values ('522.101.423-85', 'Tineja', 'M');
+
+insert into alunos values ('423.490.672-70', 'ra111');
+insert into alunos values ('814.707.732-79', 'ra222');
+insert into alunos values ('257.296.288-89', 'ra333');
+insert into alunos values ('885.516.794-49', 'ra444');
+
+insert into professores values ('906.745.318-84', '0001', 1000);
+insert into professores values ('245.868.865-95', '0002', 3000);
+insert into professores values ('289.959.114-25', '0003', 6000);
+insert into professores values ('811.737.560-51', '0004', 11000);
